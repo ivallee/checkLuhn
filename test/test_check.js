364530d5e0d0e2b14485670f40481d4a5ea83ff6
Isaac Vallee <isaacvallee@gmail.com>
2017-09-11 00:34:47 +0000

    added invalid test

--- test/test_check.js
+++ test/test_check.js
@@ -1,10 +1,18 @@
 var assert = require('chai').assert;
 var check = require('../check.js');
 
-describe("Check if Valid", function() {
+describe("Checks if number Valid", function() {
   it("Should return true if number is valid", function() {
     var number = "4485770489628630";
     var result = check(number);
     assert.isTrue(result);
+  });
+});
+
+describe("Checks if number invalid", function() {
+  it("Should return false if number is invalid", function() {
+    var number = "41234567876543219";
+    var result = check(number);
+    assert.isFalse(result);
   });
 });
